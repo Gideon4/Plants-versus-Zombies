a02edce8fbea969121bac4b6ccc9eb229741ef01
Gideon4 <43590645+Gideon4@users.noreply.github.com>
2020-02-04 19:36:08 -0600

    Week1

---
+++ Assets/Scripts/GrassMaker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassMaker : MonoBehaviour
+{
+    public GameObject grass;
+
+    void Start()
+    {
+        CreateGrass();
+    }
+
+    private void CreateGrass()
+    {
+        float grasssize = grass.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        for(int x=0; x<10; x++){
+            for(int y=0; y<5; y++){
+                GameObject newGrass = Instantiate(grass);
+                newGrass.transform.position = new Vector3(grasssize * x, grasssize * y, 0);
+            }
+        }
+    }
+}

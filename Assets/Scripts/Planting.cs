a8ce8016cb11a06433fe165d4e6f65b461199b6d
Gideon4 <43590645+Gideon4@users.noreply.github.com>
2020-03-03 19:32:05 -0600

    final

---
+++ Assets/Scripts/Planting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planting : MonoBehaviour
+{
+    void OnMouseUpAsButton(){
+        if(BuildMenu.currentplant != null){
+            Instantiate(BuildMenu.currentplant.gameObject,transform.position,Quaternion.identity);
+            SunCollection.score -= BuildMenu.currentplant.price;
+            BuildMenu.currentplant = null;
+        }
+    }
+}
---
+++ Assets/Scripts/ZombieAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAttack : MonoBehaviour
+{
+    float last = 0;
+    void OnCollisionStay2D(Collision2D coll){
+        if(coll.gameObject.tag == "Plant"){
+            GetComponent<Animator>().SetTrigger("EatingBrains");
+            if(Time.time - last >= 1){
+                coll.gameObject.GetComponent<Health>().DoDamage(1);
+                last = Time.time;
+            }
+        }
+    }
+}
